69fec2833f3b7476dd023bb41f3418bf8d6bebec
Neel <neelparihar599@gmail.com>
2024-04-13 15:56:32 +0530

    feat: added personal page
    Signed-off-by: Neel <neelparihar599@gmail.com>

---
+++ pages/personal.vue
@@ -0,0 +1,129 @@
+<template>
+
+    <div class="rounded-lg border bg-card text-card-foreground shadow-sm w-full max-w-xl mx-auto" data-v0-t="card">
+        <div class="flex flex-col space-y-1.5 p-6">
+            <div class="grid gap-1 items-center justify-center md:grid-cols-2">
+                <div class="flex items-center space-x-4">
+                    <span class="relative flex shrink-0 overflow-hidden rounded-full w-20 h-20 border"><img
+                            class="w-full object-cover" src="/personal/IMG_1457.png" /></span>
+                    <div class="">
+                        <h3 class="text-xl font-bold">Neel Parihar</h3>
+                        <div class="flex items-center space-x-2 text-sm">
+                            <span class="font-medium">23</span>
+                            <span class="opacity-70">•</span>
+                            <span class="font-medium">Mumbai</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="p-6">
+            <dl class="grid gap-2 text-sm md:grid-cols-2 md:gap-x-4 md:gap-y-2">
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Height</dt>
+                    <dd class="flex-1">5'8"</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Occupation</dt>
+                    <dd class="flex-1">Software Engineer</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Location</dt>
+                    <dd class="flex-1">Mumbai, India</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Hometown</dt>
+                    <dd class="flex-1">Salodriya, Rajasthan</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Education</dt>
+                    <dd class="flex-1">B.Tech</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Religion</dt>
+                    <dd class="flex-1">Hindu</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Zodiac sign</dt>
+                    <dd class="flex-1">Aquarius</dd>
+                </div>
+                <div class="flex items-center space-x-2">
+                    <dt class="w-20 font-semibold">Pets</dt>
+                    <dd class="flex-1">Loves dogs and cats</dd>
+                </div>
+            </dl>
+            <div class="mt-4 border-t border-gray-200 dark:border-gray-800">
+                <div class="grid gap-1.5 md:grid-cols-2 md:gap-1.5 mt-1">
+                    <div class="font-semibold">About Neel</div>
+                    <div class="prose max-w-none dark:prose-dark">
+                        I am a tech enthusiast who loves to explore new places and try different cuisines. And enjoys
+                        adventure
+                        sports and is looking for someone who can be both a travel buddy and a foodie partner.
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-4 border-t border-gray-200 dark:border-gray-800">
+                <div class="grid gap-1.5 md:grid-cols-2 md:gap-1.5 mt-1">
+                    <div class="font-semibold">Interests</div>
+                    <ul class="list-disc pl-4 grid gap-2 md:grid-cols-2 md:gap-2">
+                        <li>Travel</li>
+                        <li>Food</li>
+                        <li>Adventure sports</li>
+                        <li>Physical Fitness </li>
+                        <li>Music concerts</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="mt-4 border-t border-gray-200 dark:border-gray-800">
+                <div class="grid gap-1.5 md:grid-cols-2 md:gap-1.5 mt-1">
+                    <div class="font-semibold">Looking for</div>
+                    <ul class="list-disc pl-4 grid gap-2 md:grid-cols-2 md:gap-2">
+                        <li>Love for travel</li>
+                        <li>Foodie</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="mt-4 border-t border-gray-200 dark:border-gray-800">
+                <div class="grid gap-1.5 md:grid-cols-2 md:gap-3 mt-1">
+                    <div class="font-semibold">One thing you should know about me</div>
+                    <div class="prose max-w-none dark:prose-dark">
+                        Will always bring you a snack even if you said you didn't want one </div>
+                </div>
+            </div>
+            <div class="p-6 mt-4 border-t border-gray-200 dark:border-gray-800">
+                <div class="grid  grid-cols-1 md:grid-cols-2 gap-4 ">
+                    <img alt="Hinge Image 1" class="rounded-lg object-cover w-full" height="400"
+                        src="/personal/IMG_1457.png" style="aspect-ratio:400/400;object-fit:cover" width="400" />
+                    <img alt="Hinge Image 2" class="rounded-lg object-cover w-full" height="400"
+                        src="/personal/IMG_0676.png" style="aspect-ratio:400/400;object-fit:cover" width="400" />
+                    <img alt="Hinge Image 3" class="rounded-lg object-cover w-full" height="400"
+                        src="/personal/IMG_1170.png" style="aspect-ratio:400/400;object-fit:cover" width="400" />
+                    <img alt="Hinge Image 3" class="rounded-lg object-cover w-full" height="400"
+                        src="/personal/IMG_1472.png" style="aspect-ratio:400/400;object-fit:cover" width="400" />
+                    <img alt="Hinge Image 3" class="rounded-lg object-cover w-full" height="400"
+                        src="/personal/IMG_3523.png" style="aspect-ratio:400/400;object-fit:cover" width="400" />
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-family: 'Chivo', sans-serif;
+    --font-sans: 'Chivo';
+}
+
+body {
+    font-family: 'Libre Franklin', sans-serif;
+    --font-sans: 'Libre Franklin';
+}
+</style>
